fcd9b11210e853b6f156daab0467c2796e78218d
oibasqada <tumarbekovmax@gmail.com>
2023-03-29 15:39:37 +0600

    Kinda got how checking through .length works

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const array = [
     {src: './assets/ballon.png', id: '1'},
     {src: './assets/ballon.png', id: '2'},
     {src: './assets/ballon.png', id: '3'},
-    {src: './assets/ballon.png', id: '4'},
-    {src: './assets/ballon.png', id: '5'},
-    {src: './assets/ballon.png', id: '6'},
-    {src: './assets/ballon.png', id: '7'},
-    {src: './assets/ballon.png', id: '8'},
-    {src: './assets/ballon.png', id: '9'},
+    // {src: './assets/ballon.png', id: '4'},
+    // {src: './assets/ballon.png', id: '5'},
+    // {src: './assets/ballon.png', id: '6'},
+    // {src: './assets/ballon.png', id: '7'},
+    // {src: './assets/ballon.png', id: '8'},
+    // {src: './assets/ballon.png', id: '9'},
 ]
 
 img.innerHTML = array.map(item => {
@@ -21,12 +21,28 @@ img.innerHTML = array.map(item => {
 
 const balloon = document.querySelectorAll('img')
 
+
 balloon.forEach((item, idx) => {
+
     item.addEventListener('click', () => {
         item.classList.toggle('bgRed')
+        updateCheckbox()
+
+
+        // if (balloon.forEach(item => item.classList.contains('bgRed'))) {
+        //     return checkBox.checked = true
+        // } else if (item !== item.classList.contains('bgRed')){
+        //     return checkBox.checked = false
+        // }
     })
     checkBox.addEventListener('click', () => {
         item.classList.toggle('bgRed')
     })
 });
+
+function updateCheckbox() {
+    const checkedBalloonsCount = document.querySelectorAll('.bgRed').length;
+    checkBox.checked = checkedBalloonsCount === array.length;
+}
+
 
